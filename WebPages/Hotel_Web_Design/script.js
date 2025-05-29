cd42a86c6c613bbad34024e7a6199f313d4e949e
Manikarajhp <manikaraj480@gmail.com>
2025-05-29 15:41:16 +0530

    Script-tag

--- WebPages/Hotel_Web_Design/script.js
+++ WebPages/Hotel_Web_Design/script.js
@@ -1,8 +1,8 @@
-// document.addEventListener("keydown", (event) => {
-//     if (event.keyCode === 123 || 
-//         (event.ctrlKey && event.shiftKey && ["I", "J", "C"].includes(event.key.toUpperCase())) || 
-//         (event.ctrlKey && event.key.toUpperCase() === "U")) {
-//         event.preventDefault();
-//     }
-// });
-// document.addEventListener("contextmenu", (event) => event.preventDefault());
+document.addEventListener("keydown", (event) => {
+    if (event.keyCode === 123 || 
+        (event.ctrlKey && event.shiftKey && ["I", "J", "C"].includes(event.key.toUpperCase())) || 
+        (event.ctrlKey && event.key.toUpperCase() === "U")) {
+        event.preventDefault();
+    }
+});
+document.addEventListener("contextmenu", (event) => event.preventDefault());
