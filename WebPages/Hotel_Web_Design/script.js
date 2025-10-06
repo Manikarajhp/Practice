f70a086abffb16d8804eb1b37160c3ca0b41e93b
Manikarajhp <manikaraj480@gmail.com>
2025-10-06 16:16:08 +0530

    home-page

--- WebPages/Hotel_Web_Design/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("keydown", (event) => {
-    if (event.keyCode === 123 || 
-        (event.ctrlKey && event.shiftKey && ["I", "J", "C"].includes(event.key.toUpperCase())) || 
-        (event.ctrlKey && event.key.toUpperCase() === "U")) {
-        event.preventDefault();
-    }
-});
-document.addEventListener("contextmenu", (event) => event.preventDefault());
